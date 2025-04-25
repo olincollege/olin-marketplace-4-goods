80c39859b8979f43289a39697ef9dd50e4994772
We1chJ <weq511762338@gmail.com>
2025-04-24 20:01:53 -0400

    compelted test buy in testing commands

--- test/test_command.c
+++ test/test_command.c
@@ -17,6 +17,19 @@ Test(test_command_db, test_buy) {
   res = init_db(database);
   cr_assert_eq(res, 0, "Expected init_db to return 0, but got %d", res);
 
+  // Create a mock user
+  user new_user = {
+      .userID = 1,
+      .name = "Test User",
+      .OMG = 100,
+      .DOGE = 200,
+      .BTC = 50,
+      .ETH = 75,
+  };
+
+  res = insert_user(database, &new_user);
+  cr_assert_eq(res, 0, "Expected insert_user to return 0, but got %d", res);
+
   order* test_order = create_order(1, 1, 10, 5.0, 1);
   cr_assert_not_null(
       test_order,
@@ -24,7 +37,9 @@ Test(test_command_db, test_buy) {
 
   int buy_res = buy(database, test_order);
   cr_assert_eq(buy_res, 0, "Expected buy to return 0, but got %d", buy_res);
-  dump_database(database);
+
+  dump_database(
+      database);  // comment this out if don't want to see the database info
 
   free_order(test_order);
   close_db(database);
